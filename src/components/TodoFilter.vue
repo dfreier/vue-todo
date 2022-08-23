7e7076cff390ad7718d9b658ae5caf0a97845dd2
Dominik Freier <dominik@livingdocs.io>
2022-08-23 23:15:26 +0200

    Last component unlocked

--- src/components/TodoFilter.vue
+++ src/components/TodoFilter.vue
@@ -20,7 +20,25 @@ export default {
     Switch
   },
   setup() {
+    /**
+     * This was the last remaining component in the hierarchy that was
+     * involved into state management.
+     *
+     * Think about that.
+     * We have now the full freedom to add, remove,
+     * move components up and down in the component tree, without ever
+     * worrying about passing props and events.
+     * We can test components by mocking composables.
+     * We can test composables in isolation.
+     * We can reuse general purpose composables in many places.
+     * ...
+     */
     const { filter } = useTodo()
+
+    /**
+     * A simple writable computed to translate between
+     * the internal Boolean model and the string representation (true/false vs. all/pending)
+     */
     const checked = computed({
       get: () => filter.value === 'all',
       set: (checked) => (filter.value = checked ? 'all' : 'pending')
