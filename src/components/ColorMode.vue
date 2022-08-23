2ab7b8498ff37ba00c1cc5808e9233c89db73ad6
Dominik Freier <dominik@livingdocs.io>
2022-08-23 20:26:51 +0200

    06 - Switch between light and dark mode

---
+++ src/components/ColorMode.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    @click="next()"
+    css:h="8"
+    css:w="8"
+    css:p="1"
+    css:text="black dark:zinc-200"
+    css:rounded="full"
+    css:outline="black 2"
+  >
+    <SunIcon v-if="mode === 'light'" />
+    <MoonIcon v-if="mode === 'dark'" />
+    <DesktopComputerIcon v-if="mode === 'auto'" />
+  </button>
+</template>
+<script>
+import { DesktopComputerIcon, MoonIcon, SunIcon } from '@heroicons/vue/outline'
+import { useColorMode, useCycleList } from '@vueuse/core'
+
+export default {
+  components: {
+    DesktopComputerIcon,
+    MoonIcon,
+    SunIcon
+  },
+  setup() {
+    const mode = useColorMode({ emitAuto: true })
+    const { next } = useCycleList(['dark', 'light', 'auto'], {
+      initialValue: mode
+    })
+
+    return {
+      mode,
+      next
+    }
+  }
+}
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,5 +3,15 @@
     <h1 css:text="4xl black dark:zinc-200" css:font="bold" css:tracking="tight">
       <span css:text="green-500">Vue</span> Todos
     </h1>
+    <ColorMode />
   </header>
 </template>
+<script>
+import ColorMode from './ColorMode.vue'
+
+export default {
+  components: {
+    ColorMode
+  }
+}
+</script>
