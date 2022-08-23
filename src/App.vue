3fa21398b1e8d868aa5cb15bea2397bc5ed712b8
Dominik Freier <dominik@livingdocs.io>
2022-08-23 22:47:32 +0200

    Simplified root component

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@ import Layout from './components/Layout.vue'
 import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 
+/**
+ * No more state management in the root component! 🥳
+ */
 export default {
   components: {
     Layout,
